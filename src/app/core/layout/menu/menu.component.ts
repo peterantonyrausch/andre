54eb390724067af8e3cb0998dfbb4ed7fcee3455
AndreGartner <andre.gartner@premiersoft.net>
2023-02-21 10:52:19 -0300

    feat: created menu layout

--- src/app/core/layout/menu/menu.component.ts
+++ src/app/core/layout/menu/menu.component.ts
@@ -8,15 +8,22 @@ interface NavigationItem {
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
-  navigationList?: NavigationItem
+  navigationList?: NavigationItem[];
 
   ngOnInit() {
+    this.navigationList = this.loadNavigationList();
   }
 
   loadNavigationList() {
-
+    return [
+      { icon: 'search', routerLink: '' },
+      { icon: 'star', routerLink: '' },
+      { icon: 'sms', routerLink: '' },
+      { icon: 'sms', routerLink: '' },
+      { icon: 'account_balance', routerLink: '' },
+    ];
   }
 }
